eff0473eabbca672c4da0db503fcd4523bd3a6b8
Renan Meneses <renan.meneses@alu.ufc.br>
2020-09-03 19:26:46 -0300

    subindo modificacoes

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,12 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+
+  <h1>Fala galera</h1>
+    <p> Esse site vai ajudar de gerencia o seu ap, vamos colocar as contas em dias, <br>
+    separa as contas entre a galera ou so ajudando voce ter uma maneira de se organizar, <br>
+    uma plataforma que possa te ajudar a organizar os gasto teu ap, para que no fim do mes<br>
+    ainda sobre um pouco de verba para outras coisas <br>
+    </p>
+    <img alt="money" src="../assets/download.jpeg">
   </div>
 </template>
